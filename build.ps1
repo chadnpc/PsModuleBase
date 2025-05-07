1865124b458b391e2d480950d5679b602d3893aa
alain <chadnpc@outlook.com>
2025-05-07 11:29:36 +0200

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -1,7 +1,7 @@
 ﻿
 #!/usr/bin/env pwsh
 # .SYNOPSIS
-#   PsModuleBase buildScript v0.1.2
+#   PsModuleBase buildScript v0.1.3
 # .DESCRIPTION
 #   A custom build script for the module PsModuleBase
 # .LINK
